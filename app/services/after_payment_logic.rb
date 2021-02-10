b39a56414daac691443b0b6e2e7e345946b941e7
arozputnii <a.rpzutnii@gmail.com>
2021-02-10 13:42:59 +0200

    fix

--- app/services/after_payment_logic.rb
+++ app/services/after_payment_logic.rb
@@ -6,7 +6,7 @@ class AfterPaymentLogic
   end
 
   def call
-    if @params[:card_id].nil? && CreditCard.find_by(short_card_number: @params[:card_number].slice(-4..-1)).nil?
+    if @params[:card_id].nil? && @user.credit_cards.find_by(short_card_number: @params[:card_number].slice(-4..-1)).nil?
       credit_card = @user.credit_cards.new(short_card_number: @params[:card_number], stripe_id: @stripe_card_id)
       credit_card.save(validate: false)
     end
